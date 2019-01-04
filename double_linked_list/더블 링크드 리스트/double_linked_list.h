c8b8c11e4b4e6241ff94abf3af604e728ce81319
HWANG_YOUNG_JOON <mghysoka@gmail.com>
2019-01-04 11:58:11 +0900

    Update double_linked_list.h
    헤더파일, 함수선언

--- double_linked_list/더블 링크드 리스트/double_linked_list.h
+++ double_linked_list/더블 링크드 리스트/double_linked_list.h
@@ -1,3 +1,5 @@
+// 더블 링크드 리스트 헤더 파일 만들기
+
 #ifndef DOUBLE_LINKED_LIST_H
 #define DOUBLE_LINKED_LIST_H
 
@@ -11,9 +13,10 @@ typedef struct tag_node
 	struct tag_node* next_node;
 
 
-}node;
+}node;// 링크드 리스트와 다른 것은 앞에 있는 이전 노드를 참조하는 포인터(prev_node)가 존재한다는 것.
 typedef int element_type;
 
+// 
 node* dll_create_node(element_type new_data);
 void dll_destroy_node(node* node);
 void dll_append_node(node** head, node* new_node);
